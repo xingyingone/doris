c66b35d54177cc7bee69623d63b1440704ae8a21
xingyingone <whuxingying@163.com>
2023-08-15 16:56:16 +0800

    New aggregation function: ARRAY_AGG()
    This function requires one arguments just as ARRAY_AGG(col) and col means the column whose values you want to aggregate.
    This function Aggregates the values including NULL in a column into an array and returns a value of the ARRAY data type.
    
    Example:
    mysql> select * from test_doris;
    
    +------+------+
    
    | c1   | c2   |
    
    +------+------+
    
    |    1 | a    |
    
    |    1 | b    |
    
    |    2 | c    |
    
    |    2 | NULL |
    
    |    3 | NULL |
    
    +------+------+
    
    mysql> select c1, array_agg(c2) from test_doris group by c1;
    
    +------+-----------------+
    
    | c1   | array_agg(`c2`) |
    
    +------+-----------------+
    
    |    1 | ["a","b"]       |
    
    |    2 | [null,"c"]      |
    
    |    3 | [null]          |
    
    +------+-----------------+

---
+++ be/src/vec/aggregate_functions/aggregate_function_array.h
@@ -0,0 +1,71 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include <string.h>
+
+#include "vec/aggregate_functions/aggregate_function.h"
+#include "vec/aggregate_functions/aggregate_function_simple_factory.h"
+#include "vec/columns/column_decimal.h"
+
+namespace doris::vectorized {
+    template<typename T>
+    struct AggregateFunctionArrayAggData {
+        using ColumnType = ColumnVector<T>;
+
+        void add(const ColumnType& column,size_t offset,size_t count) { data_column.append(column, offset, count); }
+
+        ColumnType data_column; // Aggregated elements for array_agg
+    };
+
+/** Not an aggregate function, but an adapter of aggregate functions,
+  *  which any aggregate function `agg(x)` makes an aggregate function of the form `array_agg(x)`.
+  * The adapted aggregate function calculates nested aggregate function for each element of the array.
+  */
+    template<typename Data, typename T>
+    class AggregateFunctionArrayAgg final :
+            public IAggregateFunctionHelper<Data, AggregateFunctionArrayAgg<T>> {
+    private:
+
+    public:
+        using InputColumnType = ColumnVector<T>;
+
+        AggregateFunctionArrayAgg() = default;
+
+        AggregateFunctionArrayAgg(const DataType &argument_types_) : IAggregateFunctionHelper<Data,
+                AggregateFunctionArrayAgg<Data, T>>
+                                                                             (argument_types_) {}
+
+        std::string get_name() const override {
+            return "array_agg";
+        }
+
+        void create(AggregateDataPtr __restrict place) const override {
+            new (place) Data(argument_types);
+        }
+
+        void
+        add(AggregateDataPtr __restrict place, const IColumn **columns, size_t row_num, Arena *arena) const override {
+            const auto& column = down_cast<const InputColumnType&>(*columns[0]);
+            this->data(place)(column, row_num, 1);
+        }
+
+
+    }
+
+};
