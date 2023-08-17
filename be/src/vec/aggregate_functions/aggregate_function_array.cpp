a484f161f47f96375378f0aa47e0c169ed8bb9b3
xingying01 <xingying01@corp.netease.com>
2023-08-17 09:45:19 +0000

    format

--- be/src/vec/aggregate_functions/aggregate_function_array.cpp
+++ be/src/vec/aggregate_functions/aggregate_function_array.cpp
@@ -22,49 +22,47 @@
 
 namespace doris::vectorized {
 
-    template <typename K>
-    AggregateFunctionPtr create_agg_function_array_agg(const DataTypes& argument_types,
-                                                     const bool result_is_nullable) {
-        return creator_without_type::create_ignore_nullable<
-                AggregateFunctionArrayAgg<AggregateFunctionArrayAggData<K>, K>>(argument_types,
-                result_is_nullable);
-    }
+template <typename K>
+AggregateFunctionPtr create_agg_function_array_agg(const DataTypes& argument_types,
+                                                   const bool result_is_nullable) {
+    return creator_without_type::create_ignore_nullable<
+            AggregateFunctionArrayAgg<AggregateFunctionArrayAggData<K>, K>>(argument_types,
+                                                                            result_is_nullable);
+}
 
-
-    AggregateFunctionPtr create_aggregate_function_array_agg(const std::string& name,
-                                                           const DataTypes& argument_types,
-                                                           const bool result_is_nullable) {
-        WhichDataType type(remove_nullable(argument_types[0]));
+AggregateFunctionPtr create_aggregate_function_array_agg(const std::string& name,
+                                                         const DataTypes& argument_types,
+                                                         const bool result_is_nullable) {
+    WhichDataType type(remove_nullable(argument_types[0]));
 
 #define DISPATCH(TYPE)               \
     if (type.idx == TypeIndex::TYPE) \
         return create_agg_function_array_agg<TYPE>(argument_types, result_is_nullable);
 
-        FOR_NUMERIC_TYPES(DISPATCH)
-        FOR_DECIMAL_TYPES(DISPATCH)
+    FOR_NUMERIC_TYPES(DISPATCH)
+    FOR_DECIMAL_TYPES(DISPATCH)
 #undef DISPATCH
 
-        if (type.idx == TypeIndex::String) {
-            return create_agg_function_array_agg<String>(argument_types, result_is_nullable);
-        }
-        if (type.idx == TypeIndex::DateTime || type.idx == TypeIndex::Date) {
-            return create_agg_function_array_agg<Int64>(argument_types, result_is_nullable);
-        }
-        if (type.idx == TypeIndex::DateV2) {
-            return create_agg_function_array_agg<UInt32>(argument_types, result_is_nullable);
-        }
-        if (type.idx == TypeIndex::DateTimeV2) {
-            return create_agg_function_array_agg<UInt64>(argument_types, result_is_nullable);
-        }
-
-        LOG(WARNING) << fmt::format("unsupported input type {} for aggregate function {}",
-                                    argument_types[0]->get_name(), name);
-        return nullptr;
+    if (type.idx == TypeIndex::String) {
+        return create_agg_function_array_agg<String>(argument_types, result_is_nullable);
+    }
+    if (type.idx == TypeIndex::DateTime || type.idx == TypeIndex::Date) {
+        return create_agg_function_array_agg<Int64>(argument_types, result_is_nullable);
+    }
+    if (type.idx == TypeIndex::DateV2) {
+        return create_agg_function_array_agg<UInt32>(argument_types, result_is_nullable);
+    }
+    if (type.idx == TypeIndex::DateTimeV2) {
+        return create_agg_function_array_agg<UInt64>(argument_types, result_is_nullable);
     }
 
-    void register_aggregate_function_array_agg(AggregateFunctionSimpleFactory& factory) {
-        factory.register_function_both("array_agg", create_aggregate_function_array_agg);
-    }
-
-
+    LOG(WARNING) << fmt::format("unsupported input type {} for aggregate function {}",
+                                argument_types[0]->get_name(), name);
+    return nullptr;
 }
+
+void register_aggregate_function_array_agg(AggregateFunctionSimpleFactory& factory) {
+    factory.register_function_both("array_agg", create_aggregate_function_array_agg);
+}
+
+} // namespace doris::vectorized
--- be/src/vec/aggregate_functions/aggregate_function_array.h
+++ be/src/vec/aggregate_functions/aggregate_function_array.h
@@ -25,134 +25,129 @@
 //todo : need remove
 #include "vec/data_types/data_type_map.h"
 namespace doris::vectorized {
-    template<typename T>
-    struct AggregateFunctionArrayAggData {
-       // using ColumnType = ColumnVector<T>;
+template <typename T>
+struct AggregateFunctionArrayAggData {
+    // using ColumnType = ColumnVector<T>;
 
-        AggregateFunctionArrayAggData() { __builtin_unreachable(); }
+    AggregateFunctionArrayAggData() { __builtin_unreachable(); }
 
-        AggregateFunctionArrayAggData(const DataTypes& argument_types) {
-            //todo
-        }
+    AggregateFunctionArrayAggData(const DataTypes& argument_types) {
+        //todo
+    }
 
-        //void add(const ColumnType &column, size_t offset, size_t count) { /*data_column.append(column, offset, count);*/ }
+    //void add(const ColumnType &column, size_t offset, size_t count) { /*data_column.append(column, offset, count);*/ }
 
-        //ColumnType data_column; // Aggregated elements for array_agg
-    };
+    //ColumnType data_column; // Aggregated elements for array_agg
+};
 
 /** Not an aggregate function, but an adapter of aggregate functions,
   *  which any aggregate function `agg(x)` makes an aggregate function of the form `array_agg(x)`.
   * The adapted aggregate function calculates nested aggregate function for each element of the array.
   */
-    template<typename Data, typename T>
-    class AggregateFunctionArrayAgg :
-            public IAggregateFunctionDataHelper<Data, AggregateFunctionArrayAgg<Data, T>> {
+template <typename Data, typename T>
+class AggregateFunctionArrayAgg
+        : public IAggregateFunctionDataHelper<Data, AggregateFunctionArrayAgg<Data, T>> {
+public:
+    using InputColumnType = ColumnVector<T>;
 
-    public:
-        using InputColumnType = ColumnVector<T>;
+    AggregateFunctionArrayAgg() = default;
 
-        AggregateFunctionArrayAgg() = default;
+    AggregateFunctionArrayAgg(const DataTypes& argument_types_)
+            : IAggregateFunctionDataHelper<Data, AggregateFunctionArrayAgg<Data, T>>(
+                      argument_types_) {}
 
-        AggregateFunctionArrayAgg(const DataTypes &argument_types_) : IAggregateFunctionDataHelper<Data,
-                AggregateFunctionArrayAgg<Data, T>>(argument_types_) {}
+    std::string get_name() const override { return "array_agg"; }
 
-        std::string get_name() const override {
-            return "array_agg";
-        }
+    void create(AggregateDataPtr __restrict place) const override {
+        new (place) Data(argument_types);
+    }
 
-        void create(AggregateDataPtr __restrict place) const override {
-            new (place) Data(argument_types);
-        }
+    DataTypePtr get_return_type() const override {
+        //todo no map
+        return std::make_shared<DataTypeMap>(make_nullable(argument_types[0]),
+                                             make_nullable(argument_types[1]));
+    }
 
-        DataTypePtr get_return_type() const override {
-            //todo no map
-            return std::make_shared<DataTypeMap>(make_nullable(argument_types[0]),
-                                                 make_nullable(argument_types[1]));
-        }
+    void reset(AggregateDataPtr place) const override {
+        //todo
+    }
 
-        void reset(AggregateDataPtr place) const override {
-            //todo
-        }
+    void merge(AggregateDataPtr __restrict place, ConstAggregateDataPtr rhs,
+               Arena* arena) const override {
+        //todo
+    }
 
-        void merge(AggregateDataPtr __restrict place, ConstAggregateDataPtr rhs,
-                   Arena *arena) const override {
-            //todo
-        }
+    void serialize(ConstAggregateDataPtr /* __restrict place */,
+                   BufferWritable& /* buf */) const override {
+        __builtin_unreachable();
+    }
 
-        void serialize(ConstAggregateDataPtr /* __restrict place */,
-                       BufferWritable & /* buf */) const override {
-            __builtin_unreachable();
-        }
+    void deserialize(AggregateDataPtr /* __restrict place */, BufferReadable& /* buf */,
+                     Arena*) const override {
+        __builtin_unreachable();
+    }
 
-        void deserialize(AggregateDataPtr /* __restrict place */, BufferReadable & /* buf */,
-                         Arena *) const override {
-            __builtin_unreachable();
-        }
+    void add(AggregateDataPtr __restrict place, const IColumn** columns, size_t row_num,
+             Arena* arena) const override {
+        //const auto &column = down_cast<const InputColumnType &>(*columns[0]);
+        //this->data(place).add(column, row_num, 1);
+    }
 
-        void
-        add(AggregateDataPtr __restrict place, const IColumn **columns, size_t row_num, Arena *arena) const override {
-            //const auto &column = down_cast<const InputColumnType &>(*columns[0]);
-            //this->data(place).add(column, row_num, 1);
-        }
+    void streaming_agg_serialize_to_column(const IColumn** columns, MutableColumnPtr& dst,
+                                           const size_t num_rows, Arena* arena) const override {
+        //todo
+    }
 
-        void streaming_agg_serialize_to_column(const IColumn **columns, MutableColumnPtr &dst,
-                                               const size_t num_rows, Arena *arena) const override {
-            //todo
-        }
+    void deserialize_from_column(AggregateDataPtr places, const IColumn& column, Arena* arena,
+                                 size_t num_rows) const override {
+        //todo
+    }
 
-        void deserialize_from_column(AggregateDataPtr places, const IColumn &column, Arena *arena,
-                                     size_t num_rows) const override {
-            //todo
-        }
+    void serialize_to_column(const std::vector<AggregateDataPtr>& places, size_t offset,
+                             MutableColumnPtr& dst, const size_t num_rows) const override {
+        //todo
+    }
 
-        void serialize_to_column(const std::vector <AggregateDataPtr> &places, size_t offset,
-                                 MutableColumnPtr &dst, const size_t num_rows) const override {
-            //todo
-        }
+    void deserialize_and_merge_from_column(AggregateDataPtr __restrict place, const IColumn& column,
+                                           Arena* arena) const override {
+        //todo
+    }
 
-        void deserialize_and_merge_from_column(AggregateDataPtr __restrict place, const IColumn &column,
-                                               Arena *arena) const override {
-            //todo
-        }
+    void deserialize_and_merge_from_column_range(AggregateDataPtr __restrict place,
+                                                 const IColumn& column, size_t begin, size_t end,
+                                                 Arena* arena) const override {
+        //todo
+    }
 
-        void deserialize_and_merge_from_column_range(AggregateDataPtr __restrict place,
-                                                     const IColumn &column, size_t begin, size_t end,
-                                                     Arena *arena) const override {
-            //todo
-        }
+    void deserialize_and_merge_vec(const AggregateDataPtr* places, size_t offset,
+                                   AggregateDataPtr rhs, const ColumnString* column, Arena* arena,
+                                   const size_t num_rows) const override {
+        //todo
+    }
 
+    void deserialize_and_merge_vec_selected(const AggregateDataPtr* places, size_t offset,
+                                            AggregateDataPtr rhs, const ColumnString* column,
+                                            Arena* arena, const size_t num_rows) const override {
+        //todo
+    }
 
-        void deserialize_and_merge_vec(const AggregateDataPtr *places, size_t offset,
-                                       AggregateDataPtr rhs, const ColumnString *column, Arena *arena,
-                                       const size_t num_rows) const override {
-            //todo
-        }
+    void serialize_without_key_to_column(ConstAggregateDataPtr __restrict place,
+                                         IColumn& to) const override {
+        //todo
+    }
 
-        void deserialize_and_merge_vec_selected(const AggregateDataPtr *places, size_t offset,
-                                                AggregateDataPtr rhs, const ColumnString *column,
-                                                Arena *arena, const size_t num_rows) const override {
-            //todo
-        }
+    void insert_result_into(ConstAggregateDataPtr __restrict place, IColumn& to) const override {
+        //todo
+    }
 
-        void serialize_without_key_to_column(ConstAggregateDataPtr __restrict place,
-                                             IColumn &to) const override {
-            //todo
-        }
+    [[nodiscard]] MutableColumnPtr create_serialize_column() const override {
+        return get_return_type()->create_column();
+    }
 
-        void insert_result_into(ConstAggregateDataPtr __restrict place, IColumn &to) const override {
-            //todo
-        }
+    [[nodiscard]] DataTypePtr get_serialized_type() const override { return get_return_type(); }
 
-        [[nodiscard]] MutableColumnPtr create_serialize_column() const override {
-            return get_return_type()->create_column();
-        }
-
-        [[nodiscard]] DataTypePtr get_serialized_type() const override { return get_return_type(); }
-
-    protected:
-        using IAggregateFunction::argument_types;
-
-    };
+protected:
+    using IAggregateFunction::argument_types;
+};
 
 } // namespace doris::vectorized
-
